8c671353bb6981b6ad1baf3bf8740a3968062fdd
Aaryan <72549723+aaryan-clis@users.noreply.github.com>
2022-01-20 12:48:26 +0530

    Initial

---
+++ variables.js
@@ -0,0 +1,15 @@
+
+let a = 12 //we declare the a variable by 12  
+
+console.log(a) // And print variable using console.log 
+
+//  Now we will do  some maths stuff  //
+
+let  x = 15 
+
+let y  = 45
+
+let z  = x + y 
+
+console.log(z)
+
